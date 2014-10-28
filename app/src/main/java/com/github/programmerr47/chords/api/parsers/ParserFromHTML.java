c161a11d71436f6f4592a499844680101140ca98
Mikhail Spitsin <Mikhail.Spitsin@sidenis.com>
2014-10-28 10:15:48 +0400

    added parser for SongChordsSummary of Popular Chords page.

--- app/src/main/java/com/github/programmerr47/chords/api/parsers/ParserFromHTML.java
+++ app/src/main/java/com/github/programmerr47/chords/api/parsers/ParserFromHTML.java
@@ -4,11 +4,19 @@ import com.github.programmerr47.chords.representation.utils.Util;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 
 import java.io.InputStream;
 import java.util.List;
 
 /**
+ * Implements logic of working with HTML pages. Parse HTML layout and build new object(s).
+ * <br><br>
+ * <strong>Note:</strong> uses lib {@code org.jsoup} for parsing HTML.
+ * <br><br>
+ * All parsers that a using HTML layout as source for their result objects
+ * must extend this class or its inheritors.
+ *
  * @author Michael Spitsin
  * @since 2014-10-27
  */
@@ -17,50 +25,53 @@ public abstract class ParserFromHTML<ParseResult> implements ParserFrom<ParseRes
     @Override
     public ParseResult parseObjectFrom(String objectStr) {
         Document document = Jsoup.parse(objectStr);
-        return parseObjectFromDoc(document);
+        Element body = document.body();
+        return parseObjectFromDoc(body);
     }
 
     @Override
     public ParseResult parseObjectFrom(InputStream objectIS) {
         String objectStr = Util.covertInputStreamToString(objectIS);
         Document document = Jsoup.parse(objectStr);
-        return parseObjectFromDoc(document);
+        Element body = document.body();
+        return parseObjectFromDoc(body);
     }
 
     @Override
     public List<ParseResult> parseListFrom(String arrayStr) {
         Document document = Jsoup.parse(arrayStr);
-        return parseListFromDoc(document);
+        Element body = document.body();
+        return parseListFromDoc(body);
     }
 
     @Override
     public List<ParseResult> parseListFrom(InputStream arrayIS) {
         String arrayStr = Util.covertInputStreamToString(arrayIS);
         Document document = Jsoup.parse(arrayStr);
-        return parseListFromDoc(document);
+        Element body = document.body();
+        return parseListFromDoc(body);
     }
 
     /**
-     * Gets object from {@link Document} that represented handled with special lib response.
+     * Gets object from {@link Element} that represented handled with special lib response.
      * For parsing HTMLs it is decided to use external lib {@code org.jsoup}.
      * <br><br>
      * Fro more information use: http://jsoup.org/
      *
-     * @param document given document
+     * @param element given document
      * @return object if it can be even partially created (when part of document is valid and part is invalid) of null, if not
      */
-    protected abstract ParseResult parseObjectFromDoc(Document document);
+    protected abstract ParseResult parseObjectFromDoc(Element element);
 
 
     /**
-     * Gets {@link List} of objects from {@link Document} that represented response with array of result objects.
+     * Gets {@link List} of objects from {@link Element} that represented response with array of result objects.
      * For parsing HTMLs it is decided to use external lib {@code org.jsoup}.
      * <br><br>
      * Fro more information use: http://jsoup.org/
      *
-     * @param document given document
+     * @param element given document
      * @return list of objects if it can be even partially created (when part of document is valid and part is invalid) of null, if not
      */
-    //TODO seems it must be implemented here.
-    protected abstract List<ParseResult> parseListFromDoc(Document document);
+    protected abstract List<ParseResult> parseListFromDoc(Element element);
 }
